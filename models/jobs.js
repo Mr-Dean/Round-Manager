f4a20ef624db018675972e3959777c29d8eb1ba7
Mr-Dean <deanarnold0@gmail.com>
2021-08-08 12:54:42 +0100

    organise data for round show and round index, table striped styling added

--- models/jobs.js
+++ models/jobs.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const JobSchema = new Schema({
+    location: String,
     roundNumber: Number,
     ref: String,
     name: String,
--- schemas.js
+++ schemas.js
@@ -2,6 +2,7 @@ const Joi = require('joi');
 
 module.exports.jobSchema = Joi.object({
     job: Joi.object({
+        location: Joi.string().required(),
         roundNumber: Joi.number().required(),
         ref: Joi.string().required(),
         name: Joi.string().required(),
